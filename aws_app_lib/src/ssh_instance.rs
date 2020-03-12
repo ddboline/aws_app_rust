a55a19d7881faef2e5fdbef018cb655abe1df447
Daniel Boline <ddboline@gmail.com>
2020-03-12 09:33:11 -0400

    replace subprocess::Exec with tokio::process::Command

--- aws_app_lib/src/ssh_instance.rs
+++ aws_app_lib/src/ssh_instance.rs
@@ -1,13 +1,9 @@
 use anyhow::{format_err, Error};
 use lazy_static::lazy_static;
 use log::debug;
-use parking_lot::{Mutex, RwLock};
-use std::{
-    collections::HashMap,
-    io::{stdout, BufRead, BufReader, Write},
-};
-use subprocess::Exec;
-use url::Url;
+use std::collections::HashMap;
+use tokio::process::Command;
+use tokio::sync::{Mutex, RwLock};
 
 lazy_static! {
     static ref LOCK_CACHE: RwLock<HashMap<String, Mutex<()>>> = RwLock::new(HashMap::new());
@@ -21,30 +17,13 @@ pub struct SSHInstance {
 }
 
 impl SSHInstance {
-    pub fn new(user: &str, host: &str, port: u16) -> Self {
-        LOCK_CACHE.write().insert(host.into(), Mutex::new(()));
+    pub async fn new(user: &str, host: &str, port: u16) -> Self {
+        LOCK_CACHE.write().await.insert(host.into(), Mutex::new(()));
         Self {
             user: user.into(),
             host: host.into(),
             port,
         }
-    }
-
-    pub fn from_url(url: &Url) -> Result<Self, Error> {
-        let host = url.host_str().ok_or_else(|| format_err!("Parse error"))?;
-        let port = url.port().unwrap_or(22);
-        let user = url.username();
-        Ok(Self::new(user, host, port))
-    }
-
-    pub fn get_ssh_str(&self, path: &str) -> Result<String, Error> {
-        let ssh_str = if self.port == 22 {
-            format!("{}@{}:{}", self.user, self.host, path)
-        } else {
-            format!("-p {} {}@{}:{}", self.port, self.user, self.host, path)
-        };
-
-        Ok(ssh_str)
     }
 
     pub fn get_ssh_username_host(&self) -> Result<String, Error> {
@@ -57,70 +36,23 @@ impl SSHInstance {
         Ok(ssh_str)
     }
 
-    pub fn run_command_stream_stdout(&self, cmd: &str) -> Result<Vec<String>, Error> {
-        if let Some(host_lock) = LOCK_CACHE.read().get(&self.host) {
-            let _ = host_lock.lock();
+    pub async fn run_command_stream_stdout(&self, cmd: &str) -> Result<Vec<String>, Error> {
+        if let Some(host_lock) = LOCK_CACHE.read().await.get(&self.host) {
+            let _ = host_lock.lock().await;
             debug!("cmd {}", cmd);
             let user_host = self.get_ssh_username_host()?;
-            let command = format!(r#"ssh {} "{}""#, user_host, cmd);
-            let stream = Exec::shell(command).stream_stdout()?;
-            let reader = BufReader::new(stream);
-            reader
-                .lines()
-                .collect::<Result<Vec<_>, _>>()
-                .map_err(Into::into)
+
+            let output = Command::new("ssh")
+                .args(&[&user_host, "--"])
+                .args(cmd.split_whitespace())
+                .kill_on_drop(true)
+                .output()
+                .await?;
+            let output = String::from_utf8(output.stdout)?;
+            let output: Vec<_> = output.split('\n').map(|s| s.to_string()).collect();
+            Ok(output)
         } else {
             Err(format_err!("Failed to acquire lock"))
         }
-    }
-
-    pub fn run_command_print_stdout(&self, cmd: &str) -> Result<(), Error> {
-        LOCK_CACHE
-            .read()
-            .get(&self.host)
-            .ok_or_else(|| format_err!("Failed to acquire lock"))
-            .and_then(|host_lock| {
-                let _ = host_lock.lock();
-                debug!("cmd {}", cmd);
-                let user_host = self.get_ssh_username_host()?;
-                let command = format!(r#"ssh {} "{}""#, user_host, cmd);
-                let stream = Exec::shell(command).stream_stdout()?;
-                let mut reader = BufReader::new(stream);
-
-                let stdout = stdout();
-                let mut line = String::new();
-                loop {
-                    match reader.read_line(&mut line) {
-                        Ok(0) => break,
-                        Err(_) => continue,
-                        Ok(_) => {
-                            writeln!(stdout.lock(), "ssh://{}{}", user_host, line)?;
-                        }
-                    }
-                }
-                Ok(())
-            })
-    }
-
-    pub fn run_command_ssh(&self, cmd: &str) -> Result<(), Error> {
-        let user_host = self.get_ssh_username_host()?;
-        let command = format!(r#"ssh {} "{}""#, user_host, cmd);
-        self.run_command(&command)
-    }
-
-    pub fn run_command(&self, cmd: &str) -> Result<(), Error> {
-        LOCK_CACHE
-            .read()
-            .get(&self.host)
-            .ok_or_else(|| format_err!("Failed to acquire lock"))
-            .and_then(|host_lock| {
-                let _ = host_lock.lock();
-                debug!("cmd {}", cmd);
-                if Exec::shell(cmd).join()?.success() {
-                    Ok(())
-                } else {
-                    Err(format_err!("{} failed", cmd))
-                }
-            })
     }
 }
