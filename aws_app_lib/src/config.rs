ba90933ea7c6b197c88b5440bb06a67be87a001c
Daniel Boline <ddboline@gmail.com>
2020-09-15 17:23:49 -0400

    add defaults

--- aws_app_lib/src/config.rs
+++ aws_app_lib/src/config.rs
@@ -15,6 +15,7 @@ lazy_static! {
 
 #[derive(Default, Debug, Deserialize)]
 pub struct ConfigInner {
+    #[serde(default = "default_database_url")]
     pub database_url: StackString,
     #[serde(default = "default_aws_region_name")]
     pub aws_region_name: StackString,
@@ -33,10 +34,15 @@ pub struct ConfigInner {
     #[serde(default = "default_domain")]
     pub domain: StackString,
     pub novnc_path: Option<PathBuf>,
+    #[serde(default = "default_secret_path")]
     pub secret_path: PathBuf,
+    #[serde(default = "default_secret_path")]
     pub jwt_secret_path: PathBuf,
 }
 
+fn default_database_url() -> StackString {
+    "postgresql://user:password@host:1234/test_db".into()
+}
 fn default_aws_region_name() -> StackString {
     "us-east-1".into()
 }
@@ -54,6 +60,9 @@ fn default_port() -> u32 {
 }
 fn default_domain() -> StackString {
     "localhost".into()
+}
+fn default_secret_path() -> PathBuf {
+    CONFIG_DIR.join("aws_app_rust").join("secret.bin")
 }
 
 #[derive(Default, Debug, Clone)]
