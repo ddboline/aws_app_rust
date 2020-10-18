3eb154154b12c3b3b591bea7ebce082b226ccb52
Daniel Boline <ddboline@gmail.com>
2020-10-18 10:24:29 -0400

    add test for web app

--- aws_app_http/src/app.rs
+++ aws_app_http/src/app.rs
@@ -4,11 +4,12 @@ use anyhow::Error;
 use lazy_static::lazy_static;
 use std::time::Duration;
 use tokio::time::interval;
+use stack_string::StackString;
 
 use aws_app_lib::{aws_app_interface::AwsAppInterface, config::Config, pgpool::PgPool};
 
 use super::{
-    logged_user::{fill_from_db, get_secrets, SECRET_KEY, TRIGGER_DB_UPDATE},
+    logged_user::{fill_from_db, get_secrets, SECRET_KEY, TRIGGER_DB_UPDATE, KEY_LENGTH},
     routes::{
         add_user_to_group, build_spot_request, cancel_spot, cleanup_ecr_images, command,
         create_access_key, create_image, create_snapshot, create_user, delete_access_key,
@@ -28,6 +29,12 @@ pub struct AppState {
 }
 
 pub async fn start_app() -> Result<(), Error> {
+    let port = CONFIG.port;
+    get_secrets(&CONFIG.secret_path, &CONFIG.jwt_secret_path).await?;
+    run_app(&CONFIG, port, SECRET_KEY.get(), CONFIG.domain.clone()).await
+}
+
+async fn run_app(config: &Config, port: u32, cookie_secret: [u8; KEY_LENGTH], domain: StackString) -> Result<(), Error> {
     async fn _update_db(pool: PgPool) {
         let mut i = interval(Duration::from_secs(60));
         loop {
@@ -38,24 +45,20 @@ pub async fn start_app() -> Result<(), Error> {
 
     TRIGGER_DB_UPDATE.set();
 
-    get_secrets(&CONFIG.secret_path, &CONFIG.jwt_secret_path).await?;
-
-    let pool = PgPool::new(&CONFIG.database_url);
-    let aws = AwsAppInterface::new(CONFIG.clone(), pool);
+    let pool = PgPool::new(&config.database_url);
+    let aws = AwsAppInterface::new(config.clone(), pool);
 
     actix_rt::spawn(_update_db(aws.pool.clone()));
-
-    let port = aws.config.port;
 
     HttpServer::new(move || {
         App::new()
             .data(AppState { aws: aws.clone() })
             .wrap(Compress::default())
             .wrap(IdentityService::new(
-                CookieIdentityPolicy::new(&SECRET_KEY.get())
+                CookieIdentityPolicy::new(&cookie_secret)
                     .name("auth")
                     .path("/")
-                    .domain(aws.config.domain.as_str())
+                    .domain(domain.as_str())
                     .max_age(24 * 3600)
                     .secure(false),
             ))
@@ -125,4 +128,37 @@ pub async fn start_app() -> Result<(), Error> {
     .run()
     .await
     .map_err(Into::into)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::{set_var, remove_var};
+    use anyhow::Error;
+
+    use aws_app_lib::config::Config;
+
+    use crate::logged_user::{get_random_key, KEY_LENGTH};
+    use crate::app::run_app;
+
+    #[actix_rt::test]
+    async fn test_app() -> Result<(), Error> {
+        set_var("TESTENV", "true");
+        remove_var("TESTENV");
+
+        let config = Config::init_config()?;
+
+        let mut secret_key = [0u8; KEY_LENGTH];
+        secret_key.copy_from_slice(&get_random_key());
+
+        let test_port: u32 = 12345;
+        actix_rt::spawn(async move {run_app(&config, test_port, secret_key, "localhost".into()).await.unwrap()});
+        actix_rt::time::delay_for(std::time::Duration::from_secs(10)).await;
+
+        let url = format!("http://localhost:{}/aws/index.html", test_port);
+        let result = reqwest::get(&url).await?.error_for_status()?.text().await?;
+        println!("{}", result);
+        assert!(result.len() > 0);
+        assert!(result.contains("InstanceId"));
+        Ok(())
+    }
 }
--- aws_app_http/src/logged_user.rs
+++ aws_app_http/src/logged_user.rs
@@ -1,6 +1,7 @@
 use anyhow::Error;
 pub use auth_server_rust::logged_user::{
     get_secrets, LoggedUser, AUTHORIZED_USERS, JWT_SECRET, SECRET_KEY, TRIGGER_DB_UPDATE,
+    KEY_LENGTH, get_random_key,
 };
 use log::debug;
 use std::env::var;
