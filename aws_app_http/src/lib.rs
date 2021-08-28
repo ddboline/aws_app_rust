758193551fa133c8e8fac3be6eba5442c8569f84
Daniel Boline <ddboline@gmail.com>
2021-08-28 14:02:21 -0400

    refactoring

--- aws_app_http/src/lib.rs
+++ aws_app_http/src/lib.rs
@@ -17,70 +17,89 @@ pub mod requests;
 pub mod routes;
 
 use chrono::{DateTime, Utc};
-use rweb::Schema;
+use derive_more::{From, Into};
+use rweb::{
+    openapi::{ComponentDescriptor, ComponentOrInlineSchema, Entity},
+    Schema,
+};
 use serde::{Deserialize, Serialize};
 use stack_string::StackString;
-use std::collections::HashMap;
+use std::{borrow::Cow, collections::HashMap};
 
 use aws_app_lib::{
     iam_instance::{IamAccessKey, IamUser},
     resource_type::ResourceType,
 };
 
-#[derive(Debug, Serialize, Deserialize, Schema)]
-pub struct IamUserWrapper {
+#[derive(Debug, Serialize, Deserialize, Into, From)]
+pub struct IamUserWrapper(IamUser);
+
+impl Entity for IamUserWrapper {
+    fn type_name() -> Cow<'static, str> {
+        _IamUserWrapper::type_name()
+    }
+    fn describe(comp_d: &mut ComponentDescriptor) -> ComponentOrInlineSchema {
+        _IamUserWrapper::describe(comp_d)
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Schema)]
+struct _IamUserWrapper {
     #[schema(description = "Iam Arn")]
-    pub arn: StackString,
+    arn: StackString,
     #[schema(description = "Created DateTime")]
-    pub create_date: DateTime<Utc>,
+    create_date: DateTime<Utc>,
     #[schema(description = "User ID")]
-    pub user_id: StackString,
+    user_id: StackString,
     #[schema(description = "User Name")]
-    pub user_name: StackString,
+    user_name: StackString,
     #[schema(description = "Tags")]
-    pub tags: HashMap<String, StackString>,
+    tags: HashMap<String, StackString>,
 }
 
-impl From<IamUser> for IamUserWrapper {
-    fn from(item: IamUser) -> Self {
-        Self {
-            arn: item.arn,
-            create_date: item.create_date,
-            user_id: item.user_id,
-            user_name: item.user_name,
-            tags: item.tags,
-        }
+#[derive(Serialize, Deserialize, Into, From)]
+pub struct IamAccessKeyWrapper(IamAccessKey);
+
+impl Entity for IamAccessKeyWrapper {
+    fn type_name() -> Cow<'static, str> {
+        _IamAccessKeyWrapper::type_name()
+    }
+    fn describe(comp_d: &mut ComponentDescriptor) -> ComponentOrInlineSchema {
+        _IamAccessKeyWrapper::describe(comp_d)
     }
 }
 
-#[derive(Serialize, Deserialize, Schema)]
-pub struct IamAccessKeyWrapper {
+#[allow(dead_code)]
+#[derive(Schema)]
+struct _IamAccessKeyWrapper {
     #[schema(description = "Access Key ID")]
-    pub access_key_id: StackString,
+    access_key_id: StackString,
     #[schema(description = "Created DateTime")]
-    pub create_date: DateTime<Utc>,
+    create_date: DateTime<Utc>,
     #[schema(description = "Access Secret Key")]
-    pub access_key_secret: StackString,
+    access_key_secret: StackString,
     #[schema(description = "Status")]
-    pub status: StackString,
+    status: StackString,
     #[schema(description = "User Name")]
-    pub user_name: StackString,
+    user_name: StackString,
 }
 
-impl From<IamAccessKey> for IamAccessKeyWrapper {
-    fn from(item: IamAccessKey) -> Self {
-        Self {
-            access_key_id: item.access_key_id,
-            create_date: item.create_date,
-            access_key_secret: item.access_key_secret,
-            status: item.status,
-            user_name: item.user_name,
-        }
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Serialize, Deserialize, Into, From)]
+pub struct ResourceTypeWrapper(ResourceType);
+
+impl Entity for ResourceTypeWrapper {
+    fn type_name() -> Cow<'static, str> {
+        _ResourceTypeWrapper::type_name()
+    }
+    fn describe(comp_d: &mut ComponentDescriptor) -> ComponentOrInlineSchema {
+        _ResourceTypeWrapper::describe(comp_d)
     }
 }
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, Serialize, Deserialize, Schema)]
-pub enum ResourceTypeWrapper {
+#[allow(dead_code)]
+#[derive(Schema, Serialize)]
+enum _ResourceTypeWrapper {
     #[serde(rename = "instances")]
     Instances,
     #[serde(rename = "reserved")]
@@ -109,46 +128,4 @@ pub enum ResourceTypeWrapper {
     Route53,
     #[serde(rename = "systemd")]
     SystemD,
-}
-
-impl From<ResourceType> for ResourceTypeWrapper {
-    fn from(item: ResourceType) -> Self {
-        match item {
-            ResourceType::Instances => Self::Instances,
-            ResourceType::Reserved => Self::Reserved,
-            ResourceType::Spot => Self::Spot,
-            ResourceType::Ami => Self::Ami,
-            ResourceType::Volume => Self::Volume,
-            ResourceType::Snapshot => Self::Snapshot,
-            ResourceType::Ecr => Self::Ecr,
-            ResourceType::Key => Self::Key,
-            ResourceType::Script => Self::Script,
-            ResourceType::User => Self::User,
-            ResourceType::Group => Self::Group,
-            ResourceType::AccessKey => Self::AccessKey,
-            ResourceType::Route53 => Self::Route53,
-            ResourceType::SystemD => Self::SystemD,
-        }
-    }
-}
-
-impl From<ResourceTypeWrapper> for ResourceType {
-    fn from(item: ResourceTypeWrapper) -> Self {
-        match item {
-            ResourceTypeWrapper::Instances => Self::Instances,
-            ResourceTypeWrapper::Reserved => Self::Reserved,
-            ResourceTypeWrapper::Spot => Self::Spot,
-            ResourceTypeWrapper::Ami => Self::Ami,
-            ResourceTypeWrapper::Volume => Self::Volume,
-            ResourceTypeWrapper::Snapshot => Self::Snapshot,
-            ResourceTypeWrapper::Ecr => Self::Ecr,
-            ResourceTypeWrapper::Key => Self::Key,
-            ResourceTypeWrapper::Script => Self::Script,
-            ResourceTypeWrapper::User => Self::User,
-            ResourceTypeWrapper::Group => Self::Group,
-            ResourceTypeWrapper::AccessKey => Self::AccessKey,
-            ResourceTypeWrapper::Route53 => Self::Route53,
-            ResourceTypeWrapper::SystemD => Self::SystemD,
-        }
-    }
 }
