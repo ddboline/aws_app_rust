98f3b8277448b25e61d7f4e2b525ef2c73eef0ae
Daniel Boline <ddboline@gmail.com>
2021-10-30 22:57:35 -0400

    remove foreign key

--- migrations/V02__instance_list.sql
+++ migrations/V02__instance_list.sql
@@ -4,6 +4,5 @@ CREATE TABLE instance_list (
     family_name TEXT NOT NULL,
     n_cpu INT NOT NULL,
     memory_gib DOUBLE PRECISION NOT NULL,
-    generation TEXT NOT NULL,
-    FOREIGN KEY (family_name) REFERENCES instance_family(family_name)
+    generation TEXT NOT NULL
 )
