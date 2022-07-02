129d7ba0694234adfe7f90f427dcf7b71a628ba8
Daniel Boline <ddboline@gmail.com>
2022-07-02 13:25:27 -0400

    remove test

--- aws_app_lib/src/lib.rs
+++ aws_app_lib/src/lib.rs
@@ -29,30 +29,3 @@ pub mod spot_request_opt;
 pub mod ssh_instance;
 pub mod sysinfo_instance;
 pub mod systemd_instance;
-
-#[cfg(test)]
-mod tests {
-    use anyhow::Error;
-    use sysinfo::{ProcessExt, System, SystemExt};
-
-    #[test]
-    fn test_process() -> Result<(), Error> {
-        let mut sys = System::default();
-        sys.refresh_processes();
-        let procs = sys.processes_by_name("aws-app-http");
-
-        for proc in procs {
-            println!(
-                "pid:{} name:{} cpu:{} memory:{} read:{} write:{}",
-                proc.pid(),
-                proc.name(),
-                proc.cpu_usage(),
-                proc.memory(),
-                proc.disk_usage().total_read_bytes,
-                proc.disk_usage().total_written_bytes,
-            );
-        }
-        assert!(false);
-        Ok(())
-    }
-}
