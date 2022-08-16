81a54e80da77fa15ec64d4cefaa4ebc8d4c2e606
Daniel Boline <ddboline@gmail.com>
2022-08-16 07:56:51 -0400

    version bumps

--- aws_app_http/src/logged_user.rs
+++ aws_app_http/src/logged_user.rs
@@ -13,6 +13,7 @@ use std::{
     str::FromStr,
 };
 use uuid::Uuid;
+use maplit::hashset;
 
 use aws_app_lib::{models::AuthorizedUsers as AuthorizedUsersDB, pgpool::PgPool};
 
@@ -86,7 +87,7 @@ impl FromStr for LoggedUser {
 /// Returns error if `get_authorized_users` fails
 pub async fn fill_from_db(pool: &PgPool) -> Result<(), Error> {
     debug!("{:?}", *TRIGGER_DB_UPDATE);
-    let users: Vec<_> = if TRIGGER_DB_UPDATE.check() {
+    let users = if TRIGGER_DB_UPDATE.check() {
         AuthorizedUsersDB::get_authorized_users(pool)
             .await?
             .into_iter()
@@ -96,9 +97,9 @@ pub async fn fill_from_db(pool: &PgPool) -> Result<(), Error> {
         AUTHORIZED_USERS.get_users()
     };
     if let Ok("true") = var("TESTENV").as_ref().map(String::as_str) {
-        AUTHORIZED_USERS.merge_users(["user@test"]);
+        AUTHORIZED_USERS.update_users(hashset!{"user@test".into()});
     }
-    AUTHORIZED_USERS.merge_users(users);
+    AUTHORIZED_USERS.update_users(users);
     debug!("{:?}", *AUTHORIZED_USERS);
     Ok(())
 }
