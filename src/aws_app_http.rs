5a6dddf183a3253c66e49534185bd59a76f39d6f
Daniel Boline <ddboline@gmail.com>
2022-09-18 14:01:22 -0400

    version bump

--- src/aws_app_http.rs
+++ src/aws_app_http.rs
@@ -1,9 +1,12 @@
 #![allow(clippy::semicolon_if_nothing_returned)]
+use anyhow::Error;
 
 use aws_app_http::app::start_app;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     env_logger::init();
-    start_app().await.unwrap();
+    tokio::spawn(async move { start_app().await })
+        .await
+        .unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ use aws_app_lib::aws_app_opts::AwsAppOpts;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    AwsAppOpts::process_args().await
+    tokio::spawn(async move { AwsAppOpts::process_args().await })
+        .await
+        .unwrap()
 }
