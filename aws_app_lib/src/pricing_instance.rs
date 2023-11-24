522236318005fa80eea23a40e0001ee15d474de6
Daniel Boline <ddboline@gmail.com>
2023-11-24 11:03:42 -0500

    fix test

--- aws_app_lib/src/pricing_instance.rs
+++ aws_app_lib/src/pricing_instance.rs
@@ -323,7 +323,7 @@ mod tests {
         let config = aws_config::load_from_env().await;
         let pricing = PricingInstance::new(&config);
         let services = pricing.describe_services(None).await?;
-        assert_eq!(services.len(), 219);
+        assert_eq!(services.len(), 221);
         Ok(())
     }
 
